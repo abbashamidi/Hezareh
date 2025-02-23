b38acbe94a0179f63af5290df11f8767e73ed3bb
abbas <abbashamidicr@gmail.com>
2025-02-23 16:55:14 +0330

    feat:initial responsive navigation bar added ( needs more styling )

--- Hezareh.js
+++ Hezareh.js
@@ -1,11 +1,22 @@
-window.addEventListener('load', function(){
-    setTimeout(() => {
-        this.document.getElementById('preloader').style.display = 'none'
-    }, 2500);
-})
+const liList = document.getElementsByClassName("li-list");
+const sidebarList = document.getElementById("sidebar-list");
+const sideBar = document.getElementById("sidebar");
+const siderbarBtn = document.getElementById("sidebar-btn");
+const Xbtn = document.getElementById("Xicon");
 
-const menubtn = document.getElementById('menu-button');
+window.addEventListener("load", function () {
+  setTimeout(() => {
+    this.document.getElementById("preloader").style.display = "none";
+  }, 2500);
+});
 
-menubtn.addEventListener('click', ()=>{
-    alert('hey there')
-})
+siderbarBtn.addEventListener("click", function () {
+  sideBar.classList.remove("translate-x-full");
+  sideBar.classList.add("translate-x-0");
+  sideBar.classList.remove("hidden");
+});
+
+Xbtn.addEventListener("click", function () {
+  sideBar.classList.remove("translate-x-0");
+  sideBar.classList.add("translate-x-full");
+});
